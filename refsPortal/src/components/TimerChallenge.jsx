dbbf95391ea973ead38d3a68f55c7c5501cd7ec4
naplyon3 <naplyon3@hotmail.com>
2025-10-03 10:38:04 -0400

    Update

---
+++ refsPortal/src/components/TimerChallenge.jsx
@@ -0,0 +1,14 @@
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Stop</button>
+      </p>
+      <p className="active">Time is running </p>
+    </section>
+  );
+}
